58603ee34e069ea5eec115ec479a082704ed031c
Athul-k-ak <athulk8888@gmail.com>
2026-01-09 16:06:53 +0530

    import corrected

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import '../styles/authLayout.css';
+import '../styles/AuthLayout.css';
 
 const AuthLayout = () => {
     return (
