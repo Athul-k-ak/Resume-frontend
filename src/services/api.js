f9082ef98a6580acc513452e721e8a6232551aaf
Athul-k-ak <athulk8888@gmail.com>
2026-01-09 10:07:31 +0530

    backend url updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://resume-backend-jb95.onrender.com/api',
     withCredentials: true, // Important for cookies
     headers: {
         'Content-Type': 'application/json',
