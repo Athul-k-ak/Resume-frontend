1cebea104c1cb7a4c8afdb1902785633558cc593
Athul-k-ak <athulk8888@gmail.com>
2026-01-09 15:48:41 +0530

    import file name corrected

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import '../styles/AuthLayout.css';
+import '../styles/authLayout.css';
 
 const AuthLayout = () => {
     return (
